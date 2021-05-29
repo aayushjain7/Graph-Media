e12d19dbb48ec163da95a771bde603f242067524
Aayush Jain <jaayush014@gmail.com>
2021-05-30 01:24:41 +0530

    delete and single post

---
+++ client/src/components/DeleteButton.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { Button, Icon, Confirm } from 'semantic-ui-react';
+import { useMutation } from '@apollo/client';
+import { FETCH_POSTS_QUERY } from '../utils/graphql';
+
+function DeleteButton({ postId, callback }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+    update(proxy) {
+      setConfirmOpen(false);
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      let newData = data.getPosts.filter((p) => p.id !== postId);
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          ...data,
+          getPosts: {
+            newData,
+          },
+        },
+      });
+      if (callback) callback();
+    },
+    variables: { postId },
+  });
+  return (
+    <>
+      <Button as='div' color='red' floated='right' onClick={() => setConfirmOpen(true)}>
+        <Icon name='trash' style={{ margin: 0 }} />
+      </Button>
+      <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePost} />
+    </>
+  );
+}
+
+const DELETE_POST_MUTATION = gql`
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+
+export default DeleteButton;
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({ post }) {
   const { body, createdAt, id, username, likeCount, commentCount, likes } = post;
@@ -33,11 +34,7 @@ function PostCard({ post }) {
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button as='div' color='red' floated='right' onClick={() => console.log('Delete Post')}>
-            <Icon name='trash' style={{ margin: 0 }} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
