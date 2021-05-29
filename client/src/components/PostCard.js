312753f4b4ee20fccea4cadd68266e4bca5d62f1
Aayush Jain <jaayush014@gmail.com>
2021-05-30 00:42:19 +0530

    Like button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
 
 function PostCard({ post }) {
   const { body, createdAt, id, username, likeCount, commentCount, likes } = post;
+  const { user } = useContext(AuthContext);
 
   const likePost = () => {
     console.log('Like Post');
-  };
-  const commentOnPost = () => {
-    console.log('Comment Post');
   };
 
   return (
@@ -24,15 +24,8 @@ function PostCard({ post }) {
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as='div' labelPosition='right' onClick={likePost}>
-          <Button color='teal' basic>
-            <Icon name='heart' />
-          </Button>
-          <Label as='a' basic color='teal' pointing='left'>
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as='div' labelPosition='right' onClick={commentOnPost}>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
           <Button color='blue' basic>
             <Icon name='comments' />
           </Button>
@@ -40,6 +33,11 @@ function PostCard({ post }) {
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button as='div' color='red' floated='right' onClick={() => console.log('Delete Post')}>
+            <Icon name='trash' style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
